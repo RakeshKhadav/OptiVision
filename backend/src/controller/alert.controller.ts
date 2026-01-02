5c1b96d7c9ca1287f75a44d2c1829f649102f20e
RakeshKhadav <rakeshkhadav47@gmail.com>
2026-01-02 08:39:52 +0530

    feat: added GET alerts API and cleaned the code.

--- backend/src/controller/alert.controller.ts
+++ backend/src/controller/alert.controller.ts
@@ -4,6 +4,7 @@ import { ApiError } from '../utility/ApiError.js';
 import { prisma } from '../prismaClient.js';
 import { getIO } from '../services/socketService.js';
 import { uploadBase64ToCloudinary } from '../utility/cloudinaryUploadFile.js';
+import ApiResponse from '../utility/ApiResponse.js';
 
 const createAlert = asyncHandler(async (req: Request, res: Response) => {
   try {
@@ -42,14 +43,29 @@ const createAlert = asyncHandler(async (req: Request, res: Response) => {
 
     getIO().emit('alert', newAlert);
 
-    res.status(201).json({
-      success: true,
-      message: 'Alert created successfully',
-      data: newAlert,
-    });
+    res.status(201).json(new ApiResponse(201, newAlert, 'Alert created successfully'));
   } catch (error) {
     throw new ApiError(500, `Failed to create alert: ${error}`);
   }
 });
 
-export { createAlert };
+const getAlert = asyncHandler(async (req: Request, res: Response) => {
+  try {
+    const alerts = await prisma.alert.findMany({
+      select: {
+        id: true,
+        type: true,
+        severity: true,
+        message: true,
+        snapshot: true,
+        isResolved: true,
+        cameraId: true,
+      },
+    });
+    res.status(200).json(new ApiResponse(200, alerts, 'Alerts fetched successfully'));
+  } catch (error) {
+    throw new ApiError(500, `Failed to get alert: ${error}`);
+  }
+});
+
+export { createAlert, getAlert };
